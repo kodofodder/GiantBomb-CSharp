613f120a598c915a8981bd7c122fcc72007d062e
Matthew Evans <evan0415@flinders.edu.au>
2019-06-18 15:01:23 +0930

    Added gameapp

---
+++ GameApp/Pages/AddGame.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using giantbombTest.Models;
+using GiantBomb.Api.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GiantBomb.Api;
+
+namespace giantbombTest.Pages
+{
+    public class AddGameModel : PageModel
+    {
+        private readonly giantbombTest.Models.dbContext _context;
+        private readonly GiantBombRestClient _client;
+
+        public AddGameModel(giantbombTest.Models.dbContext context)
+        {
+            _context = context;
+            _client = new GiantBombRestClient("e5b38cec2a076c4dba1022ca4f95b41a8662d638");
+        }
+
+        [BindProperty]
+        public GameItem GameItem { get; set; }
+        public Game Game { get; set; }
+
+        public void OnGet(int id)
+        {
+            Game = _client.GetGame(id);
+        }
+
+        
+        public async Task<IActionResult> OnPostAsync()
+        {
+            GameItem.GbID = Game.Id;
+            GameItem.DateAdded = DateTime.Today;
+            GameItem.Name = Game.Name;
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.GameItem.Add(GameItem);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+        
+
+    }
+}
